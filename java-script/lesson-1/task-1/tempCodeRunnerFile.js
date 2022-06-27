24f17c73addf85ffcf438df9b650ead829b19872
Vybornyi <1vybornyi@gmail.com>
2022-06-27 19:43:04 +0300

    add

--- java-script/lesson-1/task-1/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-for (let i = 1; 1 <= 5; i += 1){
-    for (let x = 1; x <= 10; x += 1){
-        console.log("i + ' x ' + x =" + i * x);
-    }
-}
--- java-script/lesson-4/task-2/index.js
+++
@@ -1,9 +0,0 @@
-function uniqueCount(array){
-    if (!Array.isArray(array)){
-        return null;
-    } 
-    let  resultArray = [...new Set(array)];
-    return resultArray.length;
-}
-let chars = [1,4,1,8,8,3,4,8,8];
-console.log(unuqueCount(chars));
